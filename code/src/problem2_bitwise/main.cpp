e32dd53f26ac64a816c6a8e46f8fb724159e8eca
Georgii Zhulikov <l8doku@gmail.com>
2022-03-22 12:56:44 +0300

    Initial commit

---
+++ code/src/problem2_bitwise/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int getBitAtIndex(unsigned int number, int idx)
+{
+    int bit = 0;
+    return bit;
+}
+
+
+int countOnes(unsigned long long  number)
+{
+    int count = 0;
+    return count;
+}
+
+void printBinary(unsigned int  number)
+{
+    
+}
+
+int main()
+{
+    unsigned int  number = 9;
+    int idx = 2;
+    getBitAtIndex(number, idx);
+    countOnes(number);
+    printBinary(number);
+    return 0;
+}
