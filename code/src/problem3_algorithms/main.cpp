3332e4d7fcc60a14df6656899b151ccb5af5954d
Georgii Zhulikov <l8doku@gmail.com>
2022-04-14 14:32:37 +0300

    Workshop 26

--- code/src/problem3_algorithms/main.cpp
+++ code/src/problem3_algorithms/main.cpp
@@ -1,9 +1,11 @@
-#include <iostream>
+
+#include <algorithm>
 #include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <map>
 #include <sstream>
 #include <vector>
-#include <algorithm>
-#include <iomanip>
 
 enum class PClass
 {
@@ -22,18 +24,33 @@ enum class Sex
 
 struct Passenger
 {
+   public:
+    Passenger(int idInput) { id = idInput; }
     unsigned int id;
     bool survived;
     PClass pclass;
     std::string name;
     Sex sex;
+
     unsigned int age;
     unsigned int sibsp;
     unsigned int parch;
     std::string ticket;
+
     double fare;
     std::string cabin;
     std::string embarked;
+};
+
+struct PassengerSpecificExample : public Passenger
+{
+    std::string originCity;
+
+    PassengerSpecificExample(int idInput, std::string cityName)
+        : Passenger(idInput)
+    {
+        originCity = cityName;
+    }
 };
 
 // task 2
@@ -43,28 +60,28 @@ struct Passenger
 // 3. Data row is not full.
 enum class ErrorCode
 {
-    
+    noFile,
+    incorrectDT,
+    rowNotFull
 };
 
 // task 1
 // https://en.cppreference.com/w/cpp/error/exception
 class AppExceptExample : public std::runtime_error
 {
-    ErrorCode errorMessage;
+   public:
     // task 3
-    AppExceptExample(ErrorCode code, const char* errorMessage = "") 
-        :std::runtime_error(errorMessage)
+    ErrorCode errorCode;
+    AppExceptExample(ErrorCode code, const char* errorMessage = "")
+        : std::runtime_error(errorMessage)
     {
-        
+        errorCode = code;
     }
 };
-
-
 
 typedef std::vector<Passenger> VecPassengers;
 
 const int DEFAULT_AGE = 0;
-
 
 std::ostream& operator<<(std::ostream& out, Passenger p)
 {
@@ -83,7 +100,6 @@ std::ostream& operator<<(std::ostream& out, Passenger p)
     return out;
 }
 
-
 PClass toPClass(const std::string& buffer)
 {
     std::stringstream sspcl(buffer);
@@ -93,66 +109,66 @@ PClass toPClass(const std::string& buffer)
     return pclass;
 }
 
-
 int toInt(const std::string& buffer)
 {
     int i;
     try
     {
-      i =  std::stoi(buffer);
+        i = std::stoi(buffer);
     }
-    catch(std::invalid_argument const& ex)
+    catch (std::invalid_argument const& ex)
     {
-      std::cerr << "std::invalid_argument: " << ex.what() << "\n";
-      std::cerr << "Could not parse string: '" << buffer <<"'\n";
-      std::cerr << "Using value " << DEFAULT_AGE << " as default" << '\n';
-      i = DEFAULT_AGE;
+        std::cerr << "std::invalid_argument: " << ex.what() << "\n";
+        std::cerr << "Could not parse string: '" << buffer << "'\n";
+        std::cerr << "Using value " << DEFAULT_AGE << " as default" << '\n';
+        i = DEFAULT_AGE;
+        throw AppExceptExample(ErrorCode::incorrectDT);
     }
 
     return i;
-    
 }
-
 
 Passenger extractData(std::istream& in)
 {
-    Passenger newPassenger;
     std::string buffer;
-    
-    std::getline(in, buffer, ','); // ID
-    newPassenger.id = toInt(buffer);
-    
-    std::getline(in, buffer, ','); // survived
+
+    std::getline(in, buffer, ',');  // ID
+    Passenger newPassenger(toInt(buffer));
+
+    std::getline(in, buffer, ',');  // survived
     newPassenger.survived = buffer == "1";
-    
-    std::getline(in, buffer, ','); // pclass
+
+    std::getline(in, buffer, ',');  // pclass
     newPassenger.pclass = toPClass(buffer);
-    
-    std::getline(in, buffer, ','); // full name
+
+    std::getline(in, buffer, ',');  // full name
     newPassenger.name = buffer;
-    
-    std::getline(in, buffer, ','); // sex
-    newPassenger.sex = buffer == "male"? Sex::Male : Sex::Female;
-    
-    std::getline(in, buffer, ','); // age
+
+    std::getline(in, buffer, ',');  // sex
+    newPassenger.sex = buffer == "male" ? Sex::Male : Sex::Female;
+
+    std::getline(in, buffer, ',');  // age
     newPassenger.age = toInt(buffer);
-    
-    std::getline(in, buffer, ','); // sibsp
+
+    std::getline(in, buffer, ',');  // sibsp
     newPassenger.sibsp = toInt(buffer);
-    
-    std::getline(in, buffer, ','); // parch
+
+    std::getline(in, buffer, ',');  // parch
     newPassenger.parch = toInt(buffer);
-    
-    std::getline(in, buffer, ','); // ticket
+
+    std::getline(in, buffer, ',');  // ticket
     newPassenger.ticket = buffer;
-    
-    std::getline(in, buffer, ','); // fare
+
+    std::getline(in, buffer, ',');  // fare
     newPassenger.fare = std::stod(buffer);
 
-    std::getline(in, buffer, ','); // Cabin
+    std::getline(in, buffer, ',');  // Cabin
     newPassenger.cabin = buffer;
 
-    std::getline(in, buffer, '\n'); // Embarked
+    if(!std::getline(in, buffer, '\n'))
+    {
+        
+    }// Embarked
     newPassenger.embarked = buffer;
     return newPassenger;
 }
@@ -161,8 +177,8 @@ VecPassengers loadData(std::istream& in)
 {
     VecPassengers passengers;
     std::string buffer;
-    std::getline(in, buffer); // header
-    while(std::getline(in, buffer))
+    std::getline(in, buffer);  // header
+    while (std::getline(in, buffer))
     {
         std::stringstream lineStream(buffer);
         Passenger newPass = extractData(lineStream);
@@ -171,23 +187,52 @@ VecPassengers loadData(std::istream& in)
     return passengers;
 }
 
-
-int main ()
+int main()
 {
     const std::string INPUT_FILE_NAME = "../../data/titanic.csv";
     std::ifstream inputFile;
-    
+
     // task 4
-    try {
-    
+    std::map<ErrorCode, std::string> errorMessages = {
+        {ErrorCode::noFile, "No such file"},
+        {ErrorCode::incorrectDT, "Incorrect data type encountered"},
+        {ErrorCode::rowNotFull, "Row is not full"}};
+
+    try
+    {
         inputFile.open(INPUT_FILE_NAME);
+        if (!inputFile.is_open())
+        {
+            throw AppExceptExample(ErrorCode::noFile);
+        }
         VecPassengers passengers = loadData(inputFile);
-        std::cout << passengers[0];
+        std::cout << passengers[0] << std::endl;
     }
-//    catch (/* fill here */){
-        
-//    }
-    catch (...) {
+
+    catch (const AppExceptExample& e)
+    {
+        std::string message = errorMessages[e.errorCode];
+        std::cout << message << std::endl;
     }
     
+    // Switch example
+    
+    //    catch (const AppExceptExample& e)
+    //    {
+    //        switch (e.errorCode)
+    //        {
+    //            case ErrorCode::noFile:
+    //                std::cout << "No such file" << std::endl;
+    //                break;
+    //            case ErrorCode::incorrectDT:
+    //                std::cout << "Incorrect data type encountered" <<
+    //                std::endl; break;
+    //            case ErrorCode::rowNotFull:
+    //                std::cout << "Row is not full" << std::endl;
+    //                break;
+    //        }
+    //    }
+
+    std::cout << "All done!" << std::endl;
+    return 0;
 }
